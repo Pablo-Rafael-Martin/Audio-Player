62f817a7e6060034ba07f8ccc86c18fca9233a79
Pablo-Rafael-Martin <pablo.rafael.cfm@gmail.com>
2024-02-02 19:43:06 -0300

    Player totalmente finalizado com função de autoplay ao passar faixas

--- src/App/App.js
+++ src/App/App.js
@@ -1,58 +1,10 @@
 import './App.css';
 import Player from './Player/Player';
-import capa from '../assets/imagens/dom-casmurro.jpeg';
-import audio1 from '../assets/áudios/1.mp3';
-import audio2 from '../assets/áudios/2.mp3';
-import audio3 from '../assets/áudios/3.mp3';
-import audio4 from '../assets/áudios/4.mp3';
-import audio5 from '../assets/áudios/5.mp3';
-import audio6 from '../assets/áudios/6.mp3';
-import audio7 from '../assets/áudios/7.mp3';
-import audio8 from '../assets/áudios/8.mp3';
-import audio9 from '../assets/áudios/9.mp3';
-import audio10 from '../assets/áudios/10.mp3';
-import { useState, useRef, useEffect } from 'react'
 
 function App() {
-  const promise = [
-    {capa: capa, title: "Capítulo 1", author: 'Machado de Assis', track: audio1},
-    {capa: capa, title: "Capítulo 2", author: 'Machado de Assis', track: audio2},
-    {capa: capa, title: "Capítulo 3", author: 'Machado de Assis', track: audio3},
-    {capa: capa, title: "Capítulo 4", author: 'Machado de Assis', track: audio4},
-    {capa: capa, title: "Capítulo 5", author: 'Machado de Assis', track: audio5},
-    {capa: capa, title: "Capítulo 6", author: 'Machado de Assis', track: audio6},
-    {capa: capa, title: "Capítulo 7", author: 'Machado de Assis', track: audio7},
-    {capa: capa, title: "Capítulo 8", author: 'Machado de Assis', track: audio8},
-    {capa: capa, title: "Capítulo 9", author: 'Machado de Assis', track: audio9},
-    {capa: capa, title: "Capítulo 10", author: 'Machado de Assis', track: audio10},
-  ];
-
-  const [indexFaixaAtual, setIndexFaixaAtual] = useState(0);
-
-  function proximaFaixa() {
-    setIndexFaixaAtual((index) => (index + 1) % promise.length);
-  }
-
-  function faixaAnterior() {
-    setIndexFaixaAtual((index) => (index - 1 + promise.length) % promise.length);
-  }
-
-  const faixaAtual = promise[indexFaixaAtual];
-
-
-
-
-
   return (
     <div id="App">
-      <Player 
-      capa={faixaAtual.capa} 
-      title={faixaAtual.title} 
-      author={faixaAtual.author} 
-      track={faixaAtual.track} 
-      proximaFaixa={proximaFaixa} 
-      faixaAnterior={faixaAnterior}
-      />
+      <Player />
     </div>
   );
 }
--- src/App/Player/Player.js
+++ src/App/Player/Player.js
@@ -1,34 +1,89 @@
 import './style.css';
 import {useState, useRef, useEffect } from 'react';
+import capa from '../../assets/imagens/dom-casmurro.jpeg';
+import audio1 from '../../assets/áudios/1.mp3';
+import audio2 from '../../assets/áudios/2.mp3';
+import audio3 from '../../assets/áudios/3.mp3';
+import audio4 from '../../assets/áudios/4.mp3';
+import audio5 from '../../assets/áudios/5.mp3';
+import audio6 from '../../assets/áudios/6.mp3';
+import audio7 from '../../assets/áudios/7.mp3';
+import audio8 from '../../assets/áudios/8.mp3';
+import audio9 from '../../assets/áudios/9.mp3';
+import audio10 from '../../assets/áudios/10.mp3';
 
 
-function Player({capa, author, title, track, proximaFaixa, faixaAnterior}){
+function Player(){
+
+    // Trecho responsável por fazer mudanças de faixas
+    const promise = [
+        {capa: capa, title: "Capítulo 1", author: 'Machado de Assis', track: audio1},
+        {capa: capa, title: "Capítulo 2", author: 'Machado de Assis', track: audio2},
+        {capa: capa, title: "Capítulo 3", author: 'Machado de Assis', track: audio3},
+        {capa: capa, title: "Capítulo 4", author: 'Machado de Assis', track: audio4},
+        {capa: capa, title: "Capítulo 5", author: 'Machado de Assis', track: audio5},
+        {capa: capa, title: "Capítulo 6", author: 'Machado de Assis', track: audio6},
+        {capa: capa, title: "Capítulo 7", author: 'Machado de Assis', track: audio7},
+        {capa: capa, title: "Capítulo 8", author: 'Machado de Assis', track: audio8},
+        {capa: capa, title: "Capítulo 9", author: 'Machado de Assis', track: audio9},
+        {capa: capa, title: "Capítulo 10", author: 'Machado de Assis', track: audio10},
+      ];
+    
+    const [indexFaixaAtual, setIndexFaixaAtual] = useState(0);
+
+    function proximaFaixa() {
+        setIsPlaying(false);
+        setIndexFaixaAtual((index) => (index + 1) % promise.length);
+    }
+
+    function faixaAnterior() {
+        setIsPlaying(false);
+        setIndexFaixaAtual((index) => (index - 1 + promise.length) % promise.length);
+    }
+
+    const faixaAtual = promise[indexFaixaAtual];
+
+    useEffect(tocarFaixa, [indexFaixaAtual]);
+
+//!----------------------------------------------------------------------------------------------------
+
+    // Trecho responsável por reproduzir as faixas
     const [isPlaying, setIsPlaying] = useState(false);
     const audioRef = useRef(null);
+    
+    const [componenteMontado, setComponenteMontado] = useState(false);
+    useEffect(() => {
+        setComponenteMontado(true);
+    }, []);
 
     function tocarFaixa(){
-        if (isPlaying) {
+        if (componenteMontado){
+            if (isPlaying) {
+            console.log('isPlaying = true, pausando o áudio');
             audioRef.current.pause();
-
-        } else {
-            audioRef.current.play();
+            } else {
+                console.log('isPlaying = false, iniciando o áudio');
+                audioRef.current.play();
+            }
+            setIsPlaying(!isPlaying);
         }
-        setIsPlaying(!isPlaying);
+
     }
 
     const playerIcon = isPlaying ? "bi bi-pause-circle-fill" : "bi bi-play-circle-fill";
 
+//!----------------------------------------------------------------------------------------------------
 
-
+    // UI
     return (
         <div id='Player'>
-            <img src={capa}/>
+            <img src={faixaAtual.capa}/>
             <div className='container-audio-info'>
-                <span className='título'>{title}</span>
-                <span className='autor'>{author}</span>
+                <span className='título'>{faixaAtual.title}</span>
+                <span className='autor'>{faixaAtual.author}</span>
             </div>
 
-            <audio ref={audioRef} id='faixa-atual' src={track}></audio>
+            <audio ref={audioRef} id='faixa-atual' src={faixaAtual.track}></audio>
 
             <div id='container-botões'>
                 <button id="voltar-faixa" class="bi bi-skip-start-fill" onClick={faixaAnterior}></button>
