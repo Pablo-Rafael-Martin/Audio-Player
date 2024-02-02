d89cad7dcffa5af6f8f8597fe5e616e952205327
Pablo-Rafael-Martin <pablo.rafael.cfm@gmail.com>
2024-02-02 12:21:56 -0300

    Layout finalizado

--- src/App/Player/Player.js
+++ src/App/Player/Player.js
@@ -6,9 +6,14 @@ function Player({capa, author, title}){
     return (
         <div id='Player'>
             <img src={capa}/>
-            <div className='audio-name'>
+            <div className='container-audio-info'>
                 <span className='título'>{title}</span>
                 <span className='autor'>{author}</span>
+            </div>
+            <div id='container-botões'>
+                <button id="voltar-faixa" class="bi bi-skip-start-fill"></button>
+                <button id="tocar-faixa" class="bi bi-play-circle-fill"></button>
+                <button id="próxima-faixa" class="bi bi-skip-end-fill"></button>
             </div>
         </div>
     );
